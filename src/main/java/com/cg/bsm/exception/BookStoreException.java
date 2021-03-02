32ef22022a678f3c97822d8c3ac51f3c7c7102b3
Maddukuritarun <mmaddukuri32@gmail.com>
2021-03-02 12:35:53 +0530

    message

---
+++ src/main/java/com/cg/bsm/exception/BookStoreException.java
@@ -0,0 +1,10 @@
+package com.cg.bsm.exception;
+
+
+@SuppressWarnings("serial")
+public class BookStoreException extends Exception {
+	public BookStoreException(String errMsg) {
+		super(errMsg);
+	}
+
+}
